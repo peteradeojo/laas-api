5281f712d80d4ac89409af026dba871747f003d3
Peter Ade-Ojo <adeojopeter@gmail.com>
2023-05-15 10:42:46 +0100

    start script with render support

--- start.sh
+++ start.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/sh
 
+if [ -f "/etc/secrets/.env" ]; then
+    # code to be executed if the file is a regular file
+    echo "Env file Found, copying to dir and setting app key";
+
+    cp /etc/secrets/.env .env
+    php artisan key:generate
+else
+    # code to be executed if the file is not a regular file
+    echo "Env file Not found";
+    exit 1;
+fi
+
+
 php artisan serve --host=0.0.0.0
