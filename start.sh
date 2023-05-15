8e4d27d6f55b8eb5955eabbdbaf0d6be48990495
Peter Ade-Ojo <adeojopeter@gmail.com>
2023-05-15 10:43:36 +0100

    start script with render support

--- start.sh
+++ start.sh
@@ -2,10 +2,14 @@
 
 if [ -f "/etc/secrets/.env" ]; then
     # code to be executed if the file is a regular file
-    echo "Env file Found, copying to dir and setting app key";
+    echo "Env file Found, copying to dir and setting app key and migrate";
 
     cp /etc/secrets/.env .env
+
+    php artisan config:cache
+
     php artisan key:generate
+    php artisan migrate --force
 else
     # code to be executed if the file is not a regular file
     echo "Env file Not found";
