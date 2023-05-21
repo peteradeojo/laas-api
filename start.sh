50f2bcfb89b82bcc3a8f3651c4c4eb20c4f8e78c
Peter Ade-Ojo <adeojopeter@gmail.com>
2023-05-21 08:03:43 +0100

    docs with docusaurus

--- start.sh
+++ start.sh
@@ -1,22 +1,8 @@
 #!/usr/bin/sh
 
-# if [ -f "/etc/secrets/.env" ]; then
-#     # code to be executed if the file is a regular file
-#     echo "Env file Found, copying to dir and setting app key and migrate";
-
-#     cp /etc/secrets/.env .env
-
-#     php artisan config:cache
-
-#     php artisan key:generate
-#     php artisan migrate --force
-# else
-#     # code to be executed if the file is not a regular file
-#     echo "Env file Not found";
-#     exit 1;
-# fi
-
 sh "./render-env-check.sh"
+
+ls -la public
 
 php artisan storage:link --force
 
