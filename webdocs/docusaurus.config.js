1a8adf769f4c4ed816c400e2378de3ae6ac0e97f
Peter Ade-Ojo <peteradeojo@outlook.com>
2023-06-18 19:50:15 +0100

    config fix

--- webdocs/docusaurus.config.js
+++ webdocs/docusaurus.config.js
@@ -11,11 +11,7 @@ const config = {
     favicon: "img/favicon.ico",
 
     // Set the production url of your site here
-<<<<<<< HEAD
-    url: "https://github.com/peteradeojo/laas-api-nest",
-=======
     url: "https://lags.vercel.app",
->>>>>>> @{-1}
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
     baseUrl: "/docs/",
